c1e46da8e9e51cea13fd949f1cd4783a0e20e00d
imnllly <143996748+imnllly@users.noreply.github.com>
2023-09-04 11:47:44 +0300

    Задания 1, 2, 3

---
+++ abs.cpp
@@ -0,0 +1,12 @@
+#include "easy.h"
+using namespace std;
+
+int itc_abs(int num) {
+    cin >> num;
+    return abs(num);
+}
+
+double itc_fabs(double num) {
+    cin >> num;
+    return abs(num);
+}
---
+++ mod.cpp
@@ -0,0 +1,11 @@
+#include "easy.h"
+using namespace std;
+
+void itc_name() {
+    cout << "Nelly " << endl;
+}
+
+void itc_fio() {
+    itc_name();
+    cout << "Kaufman Vladislavovna";
+}
