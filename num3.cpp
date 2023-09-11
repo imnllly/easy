6324986a9878037fb2f3d985902039d83d43cd46
imnllly <143996748+imnllly@users.noreply.github.com>
2023-09-11 10:38:56 +0300

    Update num3.cpp

--- num3.cpp
+++ num3.cpp
@@ -4,17 +4,14 @@
 using namespace std;
 
 int itc_min(int min1, int min2) {
-	 cin >> min1 >> min2;
 	 if (min1 < min2) return min1;
 	 return min2;
 	}
 double itc_fmax(double num, double num2) {
-	cin >> num >> num2;
 	if (num > num2) return num;
 	return num2;
 }
 double itc_fmin(double num, double num2) {
-	cin >> num >> num2;
 	if (num < num2) return num;
 	return num2;
 }
