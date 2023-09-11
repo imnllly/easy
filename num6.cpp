e0609a946e37b5b326490443534da763b3b50824
imnllly <143996748+imnllly@users.noreply.github.com>
2023-09-11 10:39:53 +0300

    num6.cpp

--- num6.cpp
+++ num6.cpp
@@ -4,12 +4,10 @@
 using namespace std;
 
 bool itc_ispositive(int num) {
-	cin >> num;
 	if (num > 0) return num;
 	return !num;
 }
 bool itc_ispositive_d(double num) {
-	cin >> num;
 	if (num > 0) return num;
 	return !num;
 }
